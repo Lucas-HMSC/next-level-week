a8d4c04f76eb33887d80dbdd9cd73c73b5d4aacb
Lucas Carvalho <lucashms.carvalho@gmail.com>
2021-05-08 21:44:17 -0300

    NLW 1.0 - #2

--- nlw-1/server/src/server.ts
+++ nlw-1/server/src/server.ts
@@ -2,15 +2,40 @@ import express from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-	console.log('Listagem de usuários');
+app.use(express.json());
 
-	response.json([
-		'Diego',
-		'Lucas',
-		'Cleiton',
-		'Daniel'
-	]);
+const users = [
+	'Diego',
+	'Lucas',
+	'Cleiton',
+	'Daniel'
+]
+
+app.get('/users', (request, response) => {
+	const search = String(request.query.search);
+
+	const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+	return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+	const id = Number(request.params.id);
+
+	const user = users[id];
+
+	return response.json(user);
+})
+
+app.post('/users', (request, response) => {
+	const data = request.body;
+
+	const user = {
+		name: data.name,
+		email: data.email,
+	};
+
+	return response.json(user);
 });
 
 app.listen(3333);
