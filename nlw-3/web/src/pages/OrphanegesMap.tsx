be82e2468a308c4d0d76b7f5d48bb113266aef9b
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-05-30 17:17:49 -0300

    Update OrphanegesMap.tsx

--- nlw-3/web/src/pages/OrphanegesMap.tsx
+++ nlw-3/web/src/pages/OrphanegesMap.tsx
@@ -29,6 +29,10 @@ function OrphanegesMap() {
       <MapContainer 
         center={[-23.2994058,-45.8944638]}
         zoom={15}
+        style={{
+          width: '100%',
+          height: '100%'
+        }}
       >
         <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`} />
       </MapContainer>
