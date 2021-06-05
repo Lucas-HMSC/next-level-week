c108a2f65b172402f949428f82d1cf55d844b615
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-05 19:56:44 -0300

    NLW 4.0 - ReactJS #4

--- nlw-4/moveit-next/src/components/Profile.tsx
+++ nlw-4/moveit-next/src/components/Profile.tsx
@@ -1,6 +1,11 @@
+import { useContext } from 'react';
+import { ChallengesContext } from '../contexts/ChallengesContext';
+
 import styles from '../styles/components/Profile.module.css';
 
 export function Profile() {
+  const { level } = useContext(ChallengesContext);
+
   return (
     <div className={styles.profileContainer}>
       <img src="https://github.com/lucas-hmsc.png" alt="Lucas Carvalho" />
@@ -8,7 +13,7 @@ export function Profile() {
         <strong>Lucas Carvalho</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
-          Level 1
+          Level { level }
         </p>
       </div>
     </div>
