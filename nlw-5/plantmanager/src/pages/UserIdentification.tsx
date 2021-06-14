8287277d2dad08db75457031efd172673092408e
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-14 20:03:08 -0300

    NLW 5.0 - React Native #2

--- nlw-5/plantmanager/src/pages/UserIdentification.tsx
+++ nlw-5/plantmanager/src/pages/UserIdentification.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { 
   SafeAreaView,
   StyleSheet,
@@ -6,42 +6,80 @@ import {
   TextInput,
   View,
   KeyboardAvoidingView,
-  Platform
+  TouchableWithoutFeedback,
+  Platform,
+  Keyboard
 } from 'react-native';
+import { useNavigation } from '@react-navigation/core';
 import { Button } from '../components/Button';
 
 import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function UserIdentification() {
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
+  const [name, setName] = useState<string>();
+
+  const navigation = useNavigation();
+  
+  function handleInputBlur() {
+    setIsFocused(false);
+    setIsFilled(!!name);
+  }
+  
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+  
+  function handleInputChange(value: string) {
+    setIsFilled(!!value);
+    setName(value);
+  }
+
+  function handleSubmit() {
+    navigation.navigate('Confirmation');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <KeyboardAvoidingView
         style={styles.container}
         behavior={ Platform.OS === 'ios' ? 'padding' : 'height' }
       >
-        <View style={styles.content}>
-          <View style={styles.form}>
-            <View style={styles.header}>
-              <Text style={styles.emoji}>
-                😃
-              </Text>
-              <Text style={styles.title}>
-                Como podemos {'\n'}
-                chamar você?
-              </Text>
-            </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.content}>
+            <View style={styles.form}>
+              <View style={styles.header}>
+                <Text style={styles.emoji}>
+                  { isFilled ? '😁' : '😃'}
+                </Text>
+                <Text style={styles.title}>
+                  Como podemos {'\n'}
+                  chamar você?
+                </Text>
+              </View>
 
-            <TextInput
-              style={styles.input}
-              placeholder="Digite um nome"
-            />
+              <TextInput
+                style={[
+                  styles.input,
+                  (isFocused || isFilled) && { borderColor: colors.green }
+                ]}
+                placeholder="Digite um nome"
+                onBlur={handleInputBlur}
+                onFocus={handleInputFocus}
+                onChangeText={handleInputChange}
+              />
 
-            <View style={styles.footer}>
-              <Button />
+              <View style={styles.footer}>
+                <Button 
+                  title="Confirmar"
+                  onPress={handleSubmit}
+                />
+              </View>
             </View>
           </View>
-        </View>
+        </TouchableWithoutFeedback>
       </KeyboardAvoidingView>
     </SafeAreaView>
   )
