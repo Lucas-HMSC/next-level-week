9c61cca58b7369c4295df94c6d94f4fd7333fe80
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-06-15 21:32:28 -0300

    NLW 5.0 - React Native #3

--- nlw-5/plantmanager/src/components/Header.tsx
+++ nlw-5/plantmanager/src/components/Header.tsx
@@ -3,7 +3,9 @@ import { Image, StyleSheet, Text, View } from 'react-native';
 import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 
 import userImg from '../assets/lucas.png';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
 export function Header() {
   return (
@@ -28,13 +30,22 @@ const styles = StyleSheet.create({
     marginTop: getStatusBarHeight(),
   },
 
-  greeting: {},
+  greeting: {
+    fontSize: 32,
+    color: colors.heading,
+    fontFamily: fonts.text,
+  },
 
-  userName: {},
+  userName: {
+    fontSize: 32,
+    fontFamily: fonts.heading,
+    color: colors.heading,
+    lineHeight: 40,
+  },
 
   image: {
-    width: 80,
-    height: 80,
+    width: 70,
+    height: 70,
     borderRadius: 40,
   },
 });
