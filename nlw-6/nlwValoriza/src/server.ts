dd1ac60db499e50b6bde08157e1e00255dc48670
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-07 13:14:50 -0300

    NLW 6.0 - NodeJS #5

--- nlw-6/nlwValoriza/src/server.ts
+++ nlw-6/nlwValoriza/src/server.ts
@@ -1,17 +1,16 @@
 import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import "express-async-errors";
-
+import cors from 'cors';
 import { router } from './routes';
 
 import './database';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
 app.use(router);
-
 app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
   if (err instanceof Error) {
     return response.status(400).json({ 
