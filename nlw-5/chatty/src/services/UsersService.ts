31ef1d029fd5a64ab51aa28a4df41b3a3bbf47b8
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-09 12:04:05 -0300

    NLW 5.0 - NodeJS #3

---
+++ nlw-5/chatty/src/services/UsersService.ts
@@ -0,0 +1,36 @@
+import { getCustomRepository, Repository } from "typeorm"
+import { User } from "../entities/User";
+import { UsersRepository } from "../repositories/UsersRepository";
+
+class UsersService {
+  private usersRepository: Repository<User>;
+
+  constructor() {
+    this.usersRepository = getCustomRepository(UsersRepository);
+  }
+
+  async create(email: string) {
+    // Check if user exists
+    const userExists = await this.usersRepository.findOne({
+      email
+    });
+
+    // If exists, return user
+    if (userExists) {
+      return userExists;
+    }
+
+    // If not exists, save to DB
+    const user = this.usersRepository.create({
+      email
+    });
+
+    await this.usersRepository.save(user);
+
+    return user;
+  }
+}
+
+export {
+  UsersService
+}
