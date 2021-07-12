f7e8265b9686987aaf1074fad560a98217e83b54
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-07-12 15:37:13 -0300

    NLW 5.0 - NodeJS #5

--- nlw-5/chatty/src/http.ts
+++ nlw-5/chatty/src/http.ts
@@ -17,6 +17,10 @@ app.get('/pages/client', (request, response) => {
   return response.render('html/client.html');
 });
 
+app.get('/pages/admin', (request, response) => {
+  return response.render('html/admin.html');
+});
+
 const http = createServer(app); // Creating HTTP protocol
 const io = new Server(http);    // Creating WS protocol
 
