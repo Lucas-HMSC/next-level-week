fcb775fd4a0c30d2701b8363e5aabdbd65d743b8
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-11-21 21:48:20 -0300

    Impulse – Stage 1

---
+++ nlw-7/nodejs/src/app.js
@@ -0,0 +1,5 @@
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
+app.listen(4000, function () { return console.log("Server is running on PORT 4000"); });
