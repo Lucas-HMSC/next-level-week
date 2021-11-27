f7d601f3825265d1893cdd544de087f90b020048
Lucas Carvalho <63379937+Lucas-HMSC@users.noreply.github.com>
2021-11-26 21:32:38 -0300

    Impulse – Stage 2

--- nlw-7/reactjs/src/components/MessageList/index.tsx
+++ nlw-7/reactjs/src/components/MessageList/index.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react';
+import io from 'socket.io-client';
 import { api } from '../../services/api';
 
 import styles from './styles.module.scss';
@@ -14,8 +15,30 @@ type Message = {
   }
 }
 
+const messagesQueue: Message[] = [];
+
+const socket = io('http://localhost:4000');
+
+socket.on('new_message', (newMessage: Message) => {
+  messagesQueue.push(newMessage);
+})
+
 export function MessageList() {
   const [messages, setMessages] = useState<Message[]>([]);
+
+  useEffect(() => {
+    setInterval(() => {
+      if (messagesQueue.length > 0) {
+        setMessages(prevState => [
+          messagesQueue[0],
+          prevState[0],
+          prevState[1]
+        ].filter(Boolean));
+
+        messagesQueue.shift();
+      }
+    }, 3000)
+  }, []);
 
   useEffect(() => {
     api.get<Message[]>('messages/last3').then((response) => {
@@ -29,9 +52,9 @@ export function MessageList() {
 
       <ul className={styles.messageList}>
         {
-          messages.map((message) => {
+          messages.map((message, index) => {
             return (
-              <li className={styles.message} key={ message.id } >
+              <li className={styles.message} key={ index } >
                 <p className={styles.messageContent}>
                   { message.text }
                 </p>
