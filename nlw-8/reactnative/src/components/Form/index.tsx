49f30cafc12bbc766727fab30690c40e4f00c072
Lucas Carvalho <lucashms.carvalho@gmail.com>
2022-06-08 21:40:39 -0300

    Impulse - Stage 4

--- nlw-8/reactnative/src/components/Form/index.tsx
+++ nlw-8/reactnative/src/components/Form/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, TouchableOpacity, Text, Image, TextInput } from 'react-native';
 import { ArrowLeft } from 'phosphor-react-native';
+import { captureScreen } from 'react-native-view-shot';
+import * as FileSystem from 'expo-file-system';
 
 import { FeedbackType } from '../../components/Widget';
 import { ScreenshotButton } from '../ScreenshotButton';
@@ -8,19 +10,63 @@ import { Button } from '../Button';
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+
 import { feedbackTypes } from '../../utils/feedbackTypes';
+import { api } from '../../libs/api';
 
 interface Props {
   feedbackType: FeedbackType;
+  onFeedbackCanceled: () => void;
+  onFeedbackSent: () => void;
 }
 
-export function Form({ feedbackType }: Props) {
+export function Form({ feedbackType, onFeedbackCanceled, onFeedbackSent }: Props) {
+  const [screenshot, setScreenshot] = useState<string | null>(null);
+  const [isSendingFeedback, setIsSendingFeedback] = useState(false);
+  const [comment, setComment] = useState('');
+
   const feedbackTypeInfo = feedbackTypes[feedbackType];
+
+  function handleScreenshot() {
+    captureScreen({
+      format: 'jpg',
+      quality: 0.8
+    })
+    .then(uri => setScreenshot(uri))
+    .catch(error => console.log(error))
+  }
+
+  function handleScreenshotRemove() {
+    setScreenshot(null);
+  } 
+
+  async function handleSendFeedback() {
+    if (isSendingFeedback) {
+      return;
+    }
+
+    setIsSendingFeedback(true);
+
+    const screenshotBase64 = screenshot && await FileSystem.readAsStringAsync(screenshot, { encoding: 'base64' });
+
+    try {
+      await api.post('/feedbacks', {
+        type: feedbackType,
+        screenshot: `data:image/png;base64, ${screenshotBase64}`,
+        comment
+      });
+
+      onFeedbackSent();
+    } catch(error) {
+      console.log(error);
+      setIsSendingFeedback(false);
+    }
+  }
 
   return (
     <View style={ styles.container }>
       <View style={ styles.header }>
-        <TouchableOpacity>
+        <TouchableOpacity onPress={ onFeedbackCanceled }>
           <ArrowLeft
             size={24}
             weight='bold'
@@ -45,17 +91,20 @@ export function Form({ feedbackType }: Props) {
         style={ styles.input }
         placeholder='Algo não está funcionando bem? Queremos corrigir. Conte com detalhes o que está acontecendo...'
         placeholderTextColor={ theme.colors.text_secondary }
+        autoCorrect={false}
+        onChangeText={ setComment }
       />
 
       <View style={ styles.footer }>
         <ScreenshotButton
-          screenshot='https://github.com/lucas-hmsc.png'
-          onTakeShot={ () => {} }
-          onRemoveShot={ () => {} }
+          screenshot={ screenshot }
+          onTakeShot={ handleScreenshot }
+          onRemoveShot={ handleScreenshotRemove }
         />
 
         <Button
-          isLoading={ false }
+          onPress={ handleSendFeedback }
+          isLoading={ isSendingFeedback }
         />
       </View>
     </View>
--- nlw-8/reactnative/src/components/Success/styles.ts
+++ nlw-8/reactnative/src/components/Success/styles.ts
@@ -24,9 +24,15 @@ export const styles = StyleSheet.create({
     height: 40,
     backgroundColor: theme.colors.surface_secondary,
     borderRadius: 4,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 24,
+    marginBottom: 56,
   },
 
   buttonTitle: {
-
+    fontSize: 14,
+    fontFamily: theme.fonts.medium,
+    color: theme.colors.text_primary,
   },
 });
